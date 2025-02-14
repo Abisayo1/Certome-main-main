620352b6937d73f99b411d1fb840e10bb8c9418f
Abisayo Folu-Agoi <foluagoiabisayo@gmail.com>
2025-02-14 08:20:14 +0100

    This is an update
    Signed-off-by: Abisayo Folu-Agoi <foluagoiabisayo@gmail.com>

--- certome/src/components/BlogHeader.jsx
+++ certome/src/components/BlogHeader.jsx
@@ -12,22 +12,22 @@ const BlogHeader = () => {
 
       {/* Buttons */}
       <div className="flex flex-wrap max-md:justify-center items-center overflow-x-auto gap-3 mb-5 max-w-full">
-        <button className="outline-none py-2 px-4 rounded-3xl bg-blue border-2 border-[#797979] cursor-pointer duration-300 focus:text-blue focus:font-semibold focus:bg-blue-500">
+        <button className="outline-none py-2 px-4 rounded-3xl bg-blue border-2 border-[#797979] cursor-pointer duration-300 focus:text-white focus:font-semibold focus:bg-blue-500">
           All
         </button>
-        <button className="outline-none py-2 px-4 rounded-3xl bg-blue border-2 border-[#797979] cursor-pointer duration-300 focus:text-blue focus:font-semibold focus:bg-blue-500 min-w-[150px] sm:min-w-[200px]">
+        <button className="outline-none py-2 px-4 rounded-3xl bg-white border-2 border-[#797979] cursor-pointer duration-300 focus:text-black focus:font-semibold focus:bg-blue-500 min-w-[150px] sm:min-w-[200px]">
           Design Thinking
         </button>
-        <button className="outline-none py-2 px-4 rounded-3xl bg-blue border-2 border-[#797979] cursor-pointer duration-300 focus:text-blue focus:font-semibold focus:bg-blue-500">
+        <button className="outline-none py-2 px-4 rounded-3xl bg-white border-2 border-[#797979] cursor-pointer duration-300 focus:text-black focus:font-semibold focus:bg-blue-500">
           Technology
         </button>
-        <button className="outline-none py-2 px-4 rounded-3xl bg-blue border-2 border-[#797979] cursor-pointer duration-300 focus:text-blue focus:font-semibold focus:bg-blue-500">
+        <button className="outline-none py-2 px-4 rounded-3xl bg-white border-2 border-[#797979] cursor-pointer duration-300 focus:text-black focus:font-semibold focus:bg-blue-500">
           Web3
         </button>
-        <button className="outline-none py-2 px-4 rounded-3xl bg-blue border-2 border-[#797979] cursor-pointer duration-300 focus:text-blue focus:font-semibold focus:bg-blue-500">
+        <button className="outline-none py-2 px-4 rounded-3xl bg-white border-2 border-[#797979] cursor-pointer duration-300 focus:text-black focus:font-semibold focus:bg-blue-500">
           Programming
         </button>
-        <button className="outline-none py-2 px-4 rounded-3xl bg-blue border-2 border-[#797979] cursor-pointer duration-300 focus:text-blue focus:font-semibold focus:bg-blue-500">
+        <button className="outline-none py-2 px-4 rounded-3xl bg-white border-2 border-[#797979] cursor-pointer duration-300 focus:text-black focus:font-semibold focus:bg-blue-500">
           AI
         </button>
       </div>
@@ -38,8 +38,8 @@ const BlogHeader = () => {
           <img src="Hero.png" alt="Hero" className="w-full h-full object-cover rounded-3xl " />
 
           {/* Card */}
-          <div className="sm:absolute  mt-3 max-sm:-mt-40 max-sm:mr-10 sm:mt-0 -bottom-14 bg-blue left-5 sm:px-10 p-5 sm:pt-10 rounded-xl shadow-xl justify-start ">
-            <button className="bg-[blue] text-blue border-none outline-none py-2 px-3 text-sm rounded-lg cursor-pointer mb-3 flex ">Technology</button>
+          <div className="sm:absolute  mt-3 max-sm:-mt-40 max-sm:mr-10 sm:mt-0 -bottom-14 bg-white left-5 sm:px-10 p-5 sm:pt-10 rounded-xl shadow-xl justify-start ">
+            <button className="bg-[blue] text-white border-none outline-none py-2 px-3 text-sm rounded-lg cursor-pointer mb-3 flex ">Technology</button>
 
             {/* Wrap the title in a Link component */}
             <Link to="/blog/:id" className="text-[1em] sm:text-lg mb-3 max-w-[500px]">
